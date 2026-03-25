7b1d88cc7225021d4a11f7b471067f67bae1df8e
Patryk Niesporek <pn312381@student.polsl.pl>
2026-03-25 16:25:00 +0100

    accomplish the same in less code

---
+++ Same thing in C#.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+var regexNotMd5 = new Regex("^[0-9a-zA-Z]{32}\\..*", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+var regexImage = new Regex("\\.(png|jpg|jpeg|gif|tif|tiff)$", RegexOptions.IgnoreCase | RegexOptions.Compiled);
+bool doNotRehash = true;
+
+
+var files = new DirectoryInfo(@".").EnumerateFiles()
+	.Where(f => regexImage.IsMatch(f.Name) && (doNotRehash ? !regexNotMd5.IsMatch(f.Name) : true));
+
+foreach (var file in files)
+{
+	string hashName = await GetFileHash_MD5Async(file.FullName);
+	string newFilePathName = Path.Combine(file.DirectoryName!, hashName + Path.GetExtension(file.Name));
+	System.IO.File.Move(file.FullName!, newFilePathName);
+}
+
+
+static async Task<string> GetFileHash_MD5Async(string filePath)
+{
+	using var md5 = System.Security.Cryptography.MD5.Create();
+	using var fs = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+	var hash = await md5.ComputeHashAsync(fs);
+	fs.Close();
+	string hashName = Convert.ToHexString(hash).ToLower();
+	return hashName;
+}
---
+++ Same thing in PowerShell.ps1
@@ -0,0 +1,12 @@
+﻿Get-ChildItem -File -Path ./ | 
+    Where { $_.Name -notmatch '^[0-9a-zA-Z]{32}\..*' } | 
+    ForEach {
+        # 1. Obliczamy hash dla konkretnego pliku
+        $fileHash = (Get-FileHash $_.FullName -Algorithm MD5).Hash
+        
+        # 2. Tworzymy nową nazwę (hash + oryginalne rozszerzenie)
+        $newName = $fileHash + $_.Extension
+        
+        # 3. Zmieniamy nazwę pliku
+        Rename-Item -Path $_.FullName -NewName $newName
+    }
